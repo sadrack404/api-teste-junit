18ed5ea0e6d7449e91b20eb4935073365f677e79
sadrack404 <gabrielscr2023@gmil.com>
2023-04-26 14:37:22 -0300

    interface userServiceImpl

---
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import br.com.dicasdeumdev.api.domain.User;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+}
