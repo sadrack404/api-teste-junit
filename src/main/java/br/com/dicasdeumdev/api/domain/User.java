6a2aaf9a516b331632a746e3febc153f2552e745
sadrack404 <gabrielscr2023@gmail.com>
2023-06-12 10:33:48 -0300

    retirando o setter que nao necessario

--- src/main/java/br/com/dicasdeumdev/api/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -1,14 +1,14 @@
 package br.com.dicasdeumdev.api.domain;
 
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Getter
 @Setter
-@ToString
 @AllArgsConstructor
 @Entity(name = "usuario")
 @NoArgsConstructor
@@ -21,16 +21,4 @@ public class User {
     private String email;
     private String password;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        User user = (User) o;
-        return getId() != null && Objects.equals(getId(), user.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
